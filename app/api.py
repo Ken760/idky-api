513f9c168de1975c414d39a301ae4693ee27675d
Oleg <nehnaev@gmail.com>
2024-10-29 10:26:03 +0300

    Update api.py

--- app/api.py
+++ app/api.py
@@ -13,20 +13,41 @@ app = FastAPI(
     title='idky App'
 )
 
+# origins = [
+#     "http://79.174.86.5:8000",
+#     "http://localhost:3000",
+#     "http://localhost:8000",
+# ]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "http://localhost:8000, http://localhost:3000, http://79.174.86.5:8000"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
+
 origins = [
     "http://79.174.86.5:8000",
     "http://localhost:3000",
-    "http://localhost:8000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["*"],
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
